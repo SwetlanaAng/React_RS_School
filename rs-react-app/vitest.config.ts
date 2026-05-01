1c9b09917ba4aca465df4dae53a9e767a2eb6a4a
SwetlanaAng <93288183+SwetlanaAng@users.noreply.github.com>
2026-05-01 21:09:44 +0200

    feat: set up testing libraries

---
+++ rs-react-app/vitest.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react-swc';
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+  plugins: [tailwindcss(),react()],
+  test: {
+    globals: true,
+    include: ['**/*.test.tsx'],
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+    coverage: {
+      provider: 'v8',
+      include: ['src/**/**.{js,jsx,ts,tsx}'],
+      exclude: [
+        'src/**/*.test.{js,jsx,ts,tsx}',
+'src/**/*.spec.{js,jsx,ts,tsx}',
+'src/**/*.d.ts',
+        'src/index.{js,jsx,ts,tsx}',
+        'src/setupTests.{js,ts}',
+      ],
+      thresholds: {
+        statements: 80,
+        branches: 50,
+        functions: 50,
+        lines: 50,
+      },
+    },
+  },
+});
