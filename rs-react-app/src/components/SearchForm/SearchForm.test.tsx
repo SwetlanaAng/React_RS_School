5a041ac1cb5f73288726e172de88123fd185008a
SwetlanaAng <93288183+SwetlanaAng@users.noreply.github.com>
2026-05-02 15:29:41 +0200

    refactor: add coverage to config

--- rs-react-app/src/components/SearchForm/SearchForm.test.tsx
+++ rs-react-app/src/components/SearchForm/SearchForm.test.tsx
@@ -5,33 +5,25 @@ import userEvent from '@testing-library/user-event';
 import { mockCharacters } from '../../tests/mockCharacters';
 import { ApiService } from '../../services/apiService/apiService';
 describe('SearchForm', () => {
-    function renderSearchForm(props: SearchFormProps) {
-        return render(
-            <SearchForm  {...props}
-            />
-        );
-    }
+  function renderSearchForm(props: SearchFormProps) {
+    return render(<SearchForm {...props} />);
+  }
   it('renders SearchForm', () => {
-    renderSearchForm({onSubmit:() => {},
-        error:false,
-        search:""})
+    renderSearchForm({ onSubmit: () => {}, error: false, search: '' });
     const input = screen.getByPlaceholderText('Search...');
     expect(input).toBeInTheDocument();
     const button = screen.getByRole('button', { name: 'Search' });
     expect(button).toBeInTheDocument();
   });
   it('throws error if error is true', () => {
-    expect(()=> renderSearchForm({onSubmit:() => {},
-    error:true,
-    search:""})
-      ).toThrow('ErrorBoundary test error');
+    expect(() =>
+      renderSearchForm({ onSubmit: () => {}, error: true, search: '' })
+    ).toThrow('ErrorBoundary test error');
   });
   it('calls onSubmit when form is submitted', async () => {
     const onSubmit = vi.fn();
     const user = userEvent.setup();
-    renderSearchForm({onSubmit:onSubmit,
-        error:false,
-        search:""})
+    renderSearchForm({ onSubmit: onSubmit, error: false, search: '' });
     const button = screen.getByRole('button', { name: 'Search' });
     await user.click(button);
     expect(onSubmit).toHaveBeenCalledWith('');
@@ -39,18 +31,14 @@ describe('SearchForm', () => {
   it('calls onSubmit when form is submitted with value', async () => {
     const onSubmit = vi.fn();
     const user = userEvent.setup();
-    renderSearchForm({onSubmit:onSubmit,
-        error:false,
-        search:"Rick"})
+    renderSearchForm({ onSubmit: onSubmit, error: false, search: 'Rick' });
     const button = screen.getByRole('button', { name: 'Search' });
     await user.click(button);
     expect(onSubmit).toHaveBeenCalledWith('Rick');
   });
   it('updates input value when user types', async () => {
     const user = userEvent.setup();
-    renderSearchForm({onSubmit:() => {},
-        error:false,
-        search:""})
+    renderSearchForm({ onSubmit: () => {}, error: false, search: '' });
     const input = screen.getByPlaceholderText('Search...');
     await user.type(input, 'Rick');
     expect(input).toHaveValue('Rick');
@@ -58,27 +46,24 @@ describe('SearchForm', () => {
   it('sends request to API when form is submitted', async () => {
     const user = userEvent.setup();
     const service = new ApiService();
-    renderSearchForm({onSubmit:() => {},
-        error:false,
-        search:"Rick"})
-    
-        const button = screen.getByRole('button', { name: 'Search' });
-        await user.click(button);
+    renderSearchForm({ onSubmit: () => {}, error: false, search: 'Rick' });
+
+    const button = screen.getByRole('button', { name: 'Search' });
+    await user.click(button);
     const filteredCharacters = mockCharacters.filter((character) =>
-        character.name.includes('Rick')
-      );
-  
-      const mockFetch = vi.spyOn(globalThis, 'fetch').mockResolvedValue({
-        ok: true,
-        json: async () => ({
-          results: filteredCharacters,
-        }),
-      } as Response);
-      const result = await service.getCharacters('Rick');
-      expect(mockFetch).toHaveBeenCalledWith(
-        'https://rickandmortyapi.com/api/character/?name=Rick'
-      );
-      expect(result).toEqual(filteredCharacters);
+      character.name.includes('Rick')
+    );
+
+    const mockFetch = vi.spyOn(globalThis, 'fetch').mockResolvedValue({
+      ok: true,
+      json: async () => ({
+        results: filteredCharacters,
+      }),
+    } as Response);
+    const result = await service.getCharacters('Rick');
+    expect(mockFetch).toHaveBeenCalledWith(
+      'https://rickandmortyapi.com/api/character/?name=Rick'
+    );
+    expect(result).toEqual(filteredCharacters);
   });
-  
 });
