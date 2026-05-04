d9937d3af5642f585c5e99f8931a4dc3379101f5
SwetlanaAng <93288183+SwetlanaAng@users.noreply.github.com>
2026-05-04 12:58:39 +0200

    refactor: add lint-staged

--- rs-react-app/src/test/App.test.tsx
+++ rs-react-app/src/test/App.test.tsx
@@ -5,17 +5,15 @@ import { mockCharacters } from './mockCharacters';
 import userEvent from '@testing-library/user-event';
 
 describe('App', () => {
-    afterEach(() => {
-        vi.restoreAllMocks();
-      });
+  afterEach(() => {
+    vi.restoreAllMocks();
+  });
   it('renders App', () => {
     vi.spyOn(globalThis, 'fetch').mockResolvedValue({
-        ok: true,
-        json: async () => ({ results: mockCharacters }),
-      } as Response);
-    render(
-      <App />
-    );
+      ok: true,
+      json: async () => ({ results: mockCharacters }),
+    } as Response);
+    render(<App />);
     const input = screen.getByPlaceholderText('Search...');
     expect(input).toBeInTheDocument();
     const button = screen.getByRole('button', { name: /search/i });
@@ -23,16 +21,14 @@ describe('App', () => {
     const errorButton = screen.getByRole('button', { name: 'Error Button' });
     expect(errorButton).toBeInTheDocument();
   });
-  it('renders App with cards from API', async() => {
+  it('renders App with cards from API', async () => {
     vi.spyOn(globalThis, 'fetch').mockResolvedValue({
-        ok: true,
-        json: async () => ({ results: mockCharacters }),
-      } as Response);
-    render(
-      <App />
-    );
+      ok: true,
+      json: async () => ({ results: mockCharacters }),
+    } as Response);
+    render(<App />);
     const cardImg = await screen.findByAltText('Rick Sanchez');
-  expect(cardImg).toBeInTheDocument();
+    expect(cardImg).toBeInTheDocument();
     const cardTitle = screen.getByText('Rick Sanchez');
     expect(cardTitle).toBeInTheDocument();
     const cardImg2 = screen.getByAltText('Morty Smith');
@@ -41,26 +37,24 @@ describe('App', () => {
     expect(cardTitle2).toBeInTheDocument();
   });
   it('shows spinner while characters are loading', () => {
-    vi.spyOn(globalThis, 'fetch').mockReturnValue(
-      new Promise(() => {})
-    );
+    vi.spyOn(globalThis, 'fetch').mockReturnValue(new Promise(() => {}));
     render(<App />);
     expect(screen.getByRole('status')).toBeInTheDocument();
   });
-  it('shows error UI when API request fails', async() => {
+  it('shows error UI when API request fails', async () => {
     vi.spyOn(globalThis, 'fetch').mockResolvedValue({
-        ok: false
-      } as Response);
+      ok: false,
+    } as Response);
     render(<App />);
     expect(
-        await screen.findByText(/there is no matching characters/i)
-      ).toBeInTheDocument();
+      await screen.findByText(/there is no matching characters/i)
+    ).toBeInTheDocument();
   });
-  it('fetches characters by submitted search value', async() => {
+  it('fetches characters by submitted search value', async () => {
     const fetchMock = vi.spyOn(globalThis, 'fetch').mockResolvedValue({
-        ok: true,
-        json: async () => ({ results: []}),
-      } as Response);
+      ok: true,
+      json: async () => ({ results: [] }),
+    } as Response);
     render(<App />);
     const user = userEvent.setup();
     const input = screen.getByPlaceholderText('Search...');
@@ -68,21 +62,20 @@ describe('App', () => {
     const button = screen.getByRole('button', { name: /search/i });
     await user.click(button);
     expect(fetchMock).toHaveBeenCalledWith(
-        'https://rickandmortyapi.com/api/character/?name=Rick'
-      );
+      'https://rickandmortyapi.com/api/character/?name=Rick'
+    );
   });
-  it('shows error UI when error button clicked', async() => {
+  it('shows error UI when error button clicked', async () => {
     vi.spyOn(globalThis, 'fetch').mockResolvedValue({
-        ok: true,
-        json: async () => ({ results: [] }),
-      } as Response);
+      ok: true,
+      json: async () => ({ results: [] }),
+    } as Response);
     render(<App />);
     const user = userEvent.setup();
     const button = screen.getByRole('button', { name: /error button/i });
-    await user.click(button)
+    await user.click(button);
     expect(
-        await screen.findByText(/Something went wrong/i)
-      ).toBeInTheDocument();
+      await screen.findByText(/Something went wrong/i)
+    ).toBeInTheDocument();
   });
-
 });
