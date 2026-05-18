d9bbaa97d3bccaad6b0c3506de8421f26c4e2a0b
SwetlanaAng <93288183+SwetlanaAng@users.noreply.github.com>
2026-05-18 11:21:34 +0400

    fix: implement detailedCard showing using fetching character data

--- rs-react-app/src/components/DetailedCardRoute/DetailedCardRoute.tsx
+++ rs-react-app/src/components/DetailedCardRoute/DetailedCardRoute.tsx
@@ -1,22 +1,44 @@
-import { useOutletContext, useSearchParams } from 'react-router';
+import { useSearchParams } from 'react-router';
 import type { Character } from '../../shared/types';
 import DetailedCard from '../DetailedCard/DetailedCard';
+import { getOneCharacter } from '../../services/apiService/apiService';
+import Spinner from '../Spinner/Spinner';
+import { useEffect, useState } from 'react';
 
 export default function DetailedCardRoute() {
-  const { characters } = useOutletContext<{ characters: Character[] }>();
+  const [character, setCharacter] = useState<Character | null>(null);
   const [searchParams, setSearchParams] = useSearchParams();
 
   const detailsId = Number(searchParams.get('details'));
-  const selectedCharacter = characters.find(({ id }) => id === detailsId);
+  useEffect(() => {
+    if (!detailsId) {
+      return;
+    }
+    async function loadCharacter() {
+      try {
+        const character = await getOneCharacter(detailsId);
 
-  if (!selectedCharacter) {
+        setCharacter(character);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    void loadCharacter();
+  }, [detailsId]);
+  if (!detailsId) {
     return null;
   }
-
+  if (character?.id !== detailsId) {
+    return (
+      <div className="mx-3 my-4 self-start rounded-2xl border-2 border-teal-200 bg-white p-6 shadow-lg shadow-teal-100">
+        <Spinner />
+      </div>
+    );
+  }
   return (
     <div className="mx-3 my-4 self-start rounded-2xl border-2 border-teal-200 bg-white p-6 shadow-lg shadow-teal-100">
       <DetailedCard
-        {...selectedCharacter}
+        {...character}
         onClose={() => {
           searchParams.delete('details');
           setSearchParams(searchParams);
