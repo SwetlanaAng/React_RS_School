6b6ab5dfb7d045b32fa63c99cbb1e390ccc5b7de
SwetlanaAng <93288183+SwetlanaAng@users.noreply.github.com>
2026-05-22 00:33:34 +0400

    feat: add icon to toggle theme

--- rs-react-app/src/Layouts/Header.tsx
+++ rs-react-app/src/Layouts/Header.tsx
@@ -1,16 +1,28 @@
 import { Link } from 'react-router';
 import logo from '../assets/morty-smith.png';
+import light from '../assets/light.png';
+import dark from '../assets/dark.png';
+import { useTheme } from '../hooks/useTheme';
 
 export default function Header() {
+  const { theme, toggleTheme } = useTheme();
   return (
     <header className="flex items-center justify-between bg-teal-100 px-5 py-3">
       <Link to="/" className="">
         <img src={logo} alt="Morty" className="h-12 w-auto object-contain" />
       </Link>
-
-      <Link to="/about" className="">
-        About
-      </Link>
+      <div className="flex items-center gap-3">
+        <Link to="/about" className="">
+          About
+        </Link>
+        <button type="button" onClick={toggleTheme}>
+          <img
+            src={theme === 'light' ? dark : light}
+            alt="Sun"
+            className="h-8 w-auto object-contain cursor-pointer"
+          />{' '}
+        </button>
+      </div>
     </header>
   );
 }
--- rs-react-app/src/contexts/ThemeContext/ThemeContext.ts
+++ rs-react-app/src/contexts/ThemeContext/ThemeContext.ts
@@ -2,7 +2,7 @@ import { createContext } from 'react';
 
 interface ThemeContextType {
   theme: 'light' | 'dark';
-  setTheme: React.Dispatch<React.SetStateAction<'light' | 'dark'>>;
+  toggleTheme: () => void;
 }
 
 export const ThemeContext = createContext<ThemeContextType | null>(null);
