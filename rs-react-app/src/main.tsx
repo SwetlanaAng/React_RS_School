012b664beccbce7c3f5fdd180395ba33e626ff73
SwetlanaAng <93288183+SwetlanaAng@users.noreply.github.com>
2026-05-22 15:16:51 +0200

    feat: add store and characterSlice

--- rs-react-app/src/main.tsx
+++ rs-react-app/src/main.tsx
@@ -4,16 +4,20 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import { ThemeProvider } from './contexts/ThemeContext/ThemeContextProvider.tsx';
+import { Provider } from 'react-redux';
+import { store } from './store/store.ts';
 
 const root = document.getElementById('root');
 if (root) {
   createRoot(root).render(
     <BrowserRouter>
-      <ThemeProvider>
-        <StrictMode>
-          <App />
-        </StrictMode>
-      </ThemeProvider>
+      <StrictMode>
+        <ThemeProvider>
+          <Provider store={store}>
+            <App />
+          </Provider>
+        </ThemeProvider>
+      </StrictMode>
     </BrowserRouter>
   );
 }
---
+++ rs-react-app/src/store/charactersSlice.ts
@@ -0,0 +1,27 @@
+import { createSlice } from '@reduxjs/toolkit';
+import type { PayloadAction } from '@reduxjs/toolkit';
+import type { Character } from '../shared/types';
+
+interface CharactersState {
+  selected: Character[];
+}
+
+const initialState: CharactersState = {
+  selected: [],
+};
+
+const charactersSlice = createSlice({
+  name: 'characters',
+  initialState,
+
+  reducers: {
+    clearAllSelected: (state) => {
+      state.selected = [];
+    },
+    addCharacter: (state, action: PayloadAction<Character>) => {
+      state.selected.push(action.payload);
+    },
+  },
+});
+export const { addCharacter, clearAllSelected } = charactersSlice.actions;
+export default charactersSlice.reducer;
