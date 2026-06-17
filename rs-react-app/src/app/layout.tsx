87dd7ab1e01e6129a938c8da65a1c635fd402da6
SwetlanaAng <93288183+SwetlanaAng@users.noreply.github.com>
2026-06-17 11:14:05 +0200

    init: set next, update config files, add app folder

---
+++ rs-react-app/src/app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from 'next';
+import '../index.css';
+
+export const metadata: Metadata = {
+  title: 'Rick and Morty',
+  description: 'Rick and Morty characters search',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body id="root">{children}</body>
+    </html>
+  );
+}
